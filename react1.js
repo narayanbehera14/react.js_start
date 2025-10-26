d379f26d2c9863dc8691a93be2bbc114cd89eea2
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-10-26 10:52:08 +0530

    updated

---
+++ react1.js
@@ -0,0 +1,11 @@
+//what is react.js ?
+//js library-> UI(fronted),META facebook(2013)
+
+//why we need ract.js (in old time in facbook app example if we get any msg or frnd request count is not updated)
+//so for that we need render or reload whole page .
+// whts why component based architecture in this everything is component in react.js
+//in this everything is divide into component only that part will reload and not other component will not reload
+
+var h1 = document.createElement(h1);
+h1.innerHTML = "hello form js"
+document.body.appendChild(h1) //to access the body element
