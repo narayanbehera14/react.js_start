23205937e8c3e03c5a33203ba27f60b63092d4ea
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-11-08 11:57:32 +0530

    updated

---
+++ 02_component/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+            <h1>liju behera</h1>
+            <p>this is me youtuber</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Card
---
+++ 02_component/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='Navbar'>
+        <h1>amity</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
