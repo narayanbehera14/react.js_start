f4622235e98c4c290bdba81ab6f83a95f9c9db6d
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-11-08 17:46:09 +0530

    updated

---
+++ 03_props/03_props/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './index.css'
+import Card from './components/Card'
+
+const App = () => {
+  return (
+    <div className="parent">
+   
+    <Card/>
+    <Card/>
+ 
+      <Card/>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ 03_props/03_props/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+     <img src = "https://unsplash.com/photos/a-trail-in-the-mountains-with-trees-on-both-sides-1U7fwhyZFi0"></img>
+        <h1>hello sir</h1>
+        <p>Amity University Mumbai, Panvel</p>
+        <button>Click me</button>
+        </div>
+  )
+}
+
+export default Card
