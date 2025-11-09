e45662a1351d3259295805426d85e058991d36cb
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-11-09 19:48:12 +0530

    updated

--- 03_props/03_props/src/App.jsx
+++ 03_props/03_props/src/App.jsx
@@ -5,11 +5,30 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div className="parent">
-   
-    <Card/>
-    <Card/>
- 
-      <Card/>
+    <Card 
+     user="Aman singh" 
+     age={15} 
+     img='https://plus.unsplash.com/premium_photo-1728892768695-ebebed48ff90?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735'
+     />
+
+    <Card 
+     user="sonali" 
+     age={5} 
+     img='https://plus.unsplash.com/premium_photo-1728892768695-ebebed48ff90?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735'
+     />
+
+    <Card 
+     user="rohan" 
+     age={17} 
+     img='https://plus.unsplash.com/premium_photo-1728892768695-ebebed48ff90?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735'
+     />
+
+     <Card 
+     user="Liju" 
+     age={27} 
+     img='https://plus.unsplash.com/premium_photo-1728892768695-ebebed48ff90?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735'
+     />
+
     </div>
     
   )
--- 03_props/03_props/src/components/Card.jsx
+++ 03_props/03_props/src/components/Card.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='card'>
-     <img src = "https://unsplash.com/photos/a-trail-in-the-mountains-with-trees-on-both-sides-1U7fwhyZFi0"></img>
+     <img src = {props.img} alt={props.user}/>
+        <h1>{props.user},{props.age}</h1>
         <h1>hello sir</h1>
         <p>Amity University Mumbai, Panvel</p>
         <button>Click me</button>
