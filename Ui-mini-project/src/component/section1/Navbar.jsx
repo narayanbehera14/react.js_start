ea0111eadc383a98169fb642f0aea6fcc9603fd8
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-11-13 19:33:43 +0530

    updated

---
+++ Ui-mini-project/src/component/section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-6 px-16">
+      <h4 className='bg-black text-white px-5 py-3 rounde'>target audience</h4>
+      <button>digital platform</button>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ Ui-mini-project/src/component/section1/Section1.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../section1/Navbar";
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full bg-red-900'>
+        <Navbar/>
+    </div>
+  )
+};
+
+export default Section1
