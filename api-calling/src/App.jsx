23334b98da6c742e8c8392946bd176fc98be2711
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-12-05 22:24:55 +0530

    updated

---
+++ api-calling/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+   const getData = async () => {
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    
+    const data = await response.json()
+    console.log(data);
+
+  }
+  return (
+    <div>
+
+      <button onClick={getData}>Get Data</button>
+      
+    </div>
+  )
+}
+
+export default App
--- local-storage/src/App.jsx
+++ local-storage/src/App.jsx
@@ -2,31 +2,36 @@ import React from 'react'
 
 const App = () => {
 
-  // localStorage.clear()
-  // sessionStorage.clear()
-  // localStorage.setItem('user','Narayan')
+   // localStorage.clear()
+   // sessionStorage.clear()
+   // localStorage.setItem('user','Narayan')
 
-  const user = localStorage.getItem('user')
-   localStorage.setItem('age1','420')
-  const age1 = localStorage.getItem('age1')
+  // const user =  JSON.parse(localStorage.getItem('user'))
+  //  localStorage.setItem('age1','420')
+  // const age1 = localStorage.getItem('age1')
 
   // console.log(age1);
-  const age=  localStorage.getItem('age')
+  // const age=  localStorage.getItem('age')
 
 
-  console.log(user,age,age1);
+  // console.log(user,age,age1);
 
 
-  const user1 = {
-    username:'Liju',
-    age:18,
-    city:"Mumbai"
-  }
+const user1 = {
+  username:'Liju',
+  age:18,
+  city:"Mumbai"
+}
 
-  console.log(user1)
+localStorage.setItem('user', JSON.stringify(user1))
+const usera = JSON.parse(localStorage.getItem('user'))
+
+console.log(usera)
+
+
   return (
     <div>
-      App
+      Narayan is learning React.
       
     </div>
   )
