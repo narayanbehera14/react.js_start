13e005d0c442fa0152320ce2e273c8bcc34f11e7
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-12-08 23:40:16 +0530

    push

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -4,21 +4,41 @@ import { useEffect } from 'react';
 
 const App = () => {
  const [num , setnum] = useState(0)
+ const [num2 , setnum2] = useState(100)
+
+ function numchange () {
+  console.log('num is changing')
+ }
 
   useEffect(function () {
     console.log('use effect is running...');
-  })
+  },[num,num2])
 
-  function random () {
-    const a = Math.random()
-    console.log(a);
-  }
+  useEffect(function () {
+    numchange()
+  },[num2])
+
+  // function random () {
+  //   const a = Math.random()
+  //   console.log(a);
+  // }
   return (
     <div>
-      <h1>{num}</h1>
+      <h1>Value of num is {num} </h1>
+      <h2>Value of num2 is {num2} </h2>
       <button onClick={() => {
-        setnum(10)
+         setnum(num+1)
+        // setnum2(num2+5)
+      }} 
+      onDoubleClick={() =>  {
+        setnum2(num2+5)
       }}>click me</button>
+      <button onMouseEnter={() => {
+        setnum(num+10)
+      }}
+      onMouseLeave={() => {
+        setnum2(num2+200)
+      }}>submit me</button>
       {/* <button onClick={random}>Generate new button</button> */}
     </div>
   )
